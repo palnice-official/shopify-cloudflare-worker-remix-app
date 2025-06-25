504dcc73bddb6385b59f5fbd9a5a611c35930614
palnice-official <palnice.official@gmail.com>
2025-06-25 06:08:07 +0530

    Intial commit

--- app/db.server.ts
+++ app/db.server.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prismaGlobal: PrismaClient;
-}
+} 
 
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
